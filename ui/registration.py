65b9339e06aabe51c4830d28b4792bf00a53ceaf
Anzhalika Turlak <293852647@mail.ru>
2022-09-07 14:58:21 +0300

    add message box

--- ui/registration.py
+++ ui/registration.py
@@ -3,9 +3,10 @@ import sys
 import sqlite3
 # шифруем пароль
 import hashlib
+from string import octdigits
 
 from PySide6.QtCore import QDate
-from PySide6.QtWidgets import QApplication, QHeaderView, QComboBox, QFormLayout, QWidget
+from PySide6.QtWidgets import QApplication, QHeaderView, QComboBox, QFormLayout, QWidget, QMessageBox
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout
 
@@ -25,14 +26,21 @@ class Regisrtation(QWidget):
 
         self.ui.pushButton.clicked.connect(self.verification)
         self.ui.pushButton_2.clicked.connect(self.registration)
+        # после вызова функции очистить
+        self.ui.lineEdit.clear()
+        self.ui.lineEdit_2.clear()
+        self.ui.lineEdit_3.clear()
 
     def coding_pass(self, val):
         return hashlib.md5(val.encode()).hexdigest()
 
+    def message_info(self, title, message):
+        self.massage = QMessageBox().information(window, title, message)
+
     def verification(self):
         self.login = self.ui.lineEdit.text()
         self.password = self.ui.lineEdit_2.text()
-        print(self.login, self.password)
+        #print(self.login, self.password)
 
         try:
             # подключаемся к базе
@@ -66,28 +74,52 @@ class Regisrtation(QWidget):
         self.password = self.ui.lineEdit_2.text()
         self.email = self.ui.lineEdit_3.text()
         self.users_values = (self.login, self.password, self.email)
-        print(self.login, self.password, self.email)
+        #print(self.login, self.password, self.email)
 
-        # подключаемся к базе
-        db = sqlite3.connect('../database.db')
-        cursor = db.cursor()
+        if self.login[0].isupper() and len(self.login) > 3:
+            self.ui.lineEdit.setStyleSheet("background-color: rgb(0, 212, 155);")
+            if len(self.password) > 5:
+                self.total_upper = False
+                self.total_lower = False
+                self.total_digit = False
+                for i in self.password:
+                    if i.isupper():
+                        self.total_upper = True
+                    elif i.islower():
+                        self.total_lower = True
+                    elif i.isdigit():
+                        self.total_digit = True
+                if self.total_upper and self.total_lower and self.total_digit:
+                    print(self.password)
+                    self.ui.lineEdit_2.setStyleSheet("background-color: rgb(0, 212, 155);")
+                    if '@' in self.email and '.' in self.email and len(self.email) > 4:
+                        self.ui.lineEdit_3.setStyleSheet("background-color: rgb(0, 212, 155);")
+                        # подключаемся к базе
+                        db = sqlite3.connect('../database.db')
+                        cursor = db.cursor()
 
-        # передаем функцию в SQL аргументы: 1-алиаса, 2-кол значений, 3 сама функция
-        db.create_function("coding_pass", 1, self.coding_pass)
+                        # передаем функцию в SQL аргументы: 1-алиаса, 2-кол значений, 3 сама функция
+                        db.create_function("coding_pass", 1, self.coding_pass)
 
-        cursor.execute("INSERT INTO users(user_login, user_password, user_email) VALUES (?,coding_pass(?),?)", self.users_values)
-        db.commit()
-        cursor.execute("SELECT * FROM users")
-        k = cursor.fetchall()
-        print(k)
+                        cursor.execute(
+                            "INSERT INTO users(user_login, user_password, user_email) VALUES (?,coding_pass(?),?)",
+                            self.users_values)
+                        db.commit()
+                        # cursor.execute("SELECT * FROM users")
+                        # k = cursor.fetchall()
+                        # print(k)
+                        cursor.close()
+                        db.close()
+                        self.message_info('Registration', f'Пользователь {self.login} успешно зарегистрирован!')
+                    else:
+                        self.message_info('Registration email address', 'Почта говно')
+                else:
+                    self.message_info('Registration password', 'Пароль должен содержать 1 цифру Букву Верх и букву ниж')
+            else:
+                self.message_info('Registration password','Пароль короткий')
+        else:
+            self.message_info('Registration login', 'Логин начинается с большой буквы не менее 4 символов')
 
-
-        cursor.close()
-        db.close()
-
-        self.ui.lineEdit.clear()
-        self.ui.lineEdit_2.clear()
-        self.ui.lineEdit_3.clear()
         self.users_values = []
 
 
